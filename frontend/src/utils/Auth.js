1a0f3a34328685ad276003d614d51447ebc8f880
Kirill <117633274+04Fox04@users.noreply.github.com>
2023-07-30 16:12:19 +0300

    поменял адреса

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,4 +1,5 @@
-export const BASE_URL = 'http://localhost:3000';
+// export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'https://backend.domainname.nomoreparties.co';
 
 function checkResponse(res) {
     if (res.ok) {

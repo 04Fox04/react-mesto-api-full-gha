8b39ccf51a30f63d039ea4bed42634d08d1d063c
Kirill <117633274+04Fox04@users.noreply.github.com>
2023-07-30 22:26:39 +0300

    test

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -1,113 +1,14 @@
-// class Api {
-//   constructor({ url, headers }) {
-//     this._url = url
-//     this._headers = headers
-//   }
-
-//   getInitialCards() {
-//     return fetch(this._url + "/cards", {
-//       method: "GET",
-//       credentials: 'include',
-//       headers: this._headers
-//     }).then((res) => this._response(res))
-//   }
-//   _response(res) {
-//     if (res.ok) {
-//       return res.json()
-//     }
-//     return Promise.reject(`Ошибка: ${res.status}`)
-//   }
-//   getInitialUser() {
-//     return fetch(this._url + "/users/me", {
-//       method: "GET",
-//       credentials: 'include',
-//       headers: this._headers
-//     }).then((res) => this._response(res))
-//   }
-//   getEditUser(data) {
-//     return fetch(this._url + "/users/me", {
-//       method: "PATCH",
-//       credentials: 'include',
-//       headers: this._headers,
-//       body: JSON.stringify({
-//         name: data.name,
-//         about: data.about
-//       })
-//     }).then((res) => this._response(res))
-//   }
-//   getEditAvatar(link) {
-//     return fetch(this._url + "/users/me/avatar", {
-//       method: "PATCH",
-//       credentials: 'include',
-//       headers: this._headers,
-//       body: JSON.stringify({
-//         avatar: link.avatar
-//       })
-//     }).then((res) => this._response(res))
-//   }
-//   getAddCard(data) {
-//     return fetch(this._url + "/cards", {
-//       method: "POST",
-//       credentials: 'include',
-//       headers: this._headers,
-//       body: JSON.stringify({
-//         name: data.name,
-//         link: data.link
-//       })
-//     }).then((res) => this._response(res))
-//   }
-//   deleteCards(cardId) {
-//     return fetch(this._url + "/cards/" + cardId, {
-//       method: "DELETE",
-//       credentials: 'include',
-//       headers: this._headers
-//     }).then((res) => this._response(res))
-//   }
-//   deleteLike(cardId) {
-//     return fetch(this._url + "/cards/" + cardId + "/likes", {
-//       method: "DELETE",
-//       credentials: 'include',
-//       headers: this._headers
-//     }).then((res) => this._response(res))
-//   }
-//   addLike(cardId) {
-//     return fetch(this._url + "/cards/" + cardId + "/likes", {
-//       method: "PUT",
-//       credentials: 'include',
-//       headers: this._headers
-//     }).then((res) => this._response(res))
-//   }
-//   changeLikeCardStatus(cardId, isLiked) {
-//     if (isLiked) {
-//       return this.addLike(cardId)
-//     } else {
-//       return this.deleteLike(cardId)
-//     }
-//   }
-// }
-
-// export const api = new Api({
-//   // url: "https://mesto.nomoreparties.co/v1/cohort-64",
-//   // url: 'http://localhost:3000',
-//   url: 'https://backend.domainname.nomoreparties.co',
-//   headers: {
-//     authorization: '',
-//     "content-type": 'application/json',
-//   },
-// });
-
 class Api {
-  constructor({ url }) {
+  constructor({ url, headers }) {
     this._url = url
+    this._headers = headers
   }
 
   getInitialCards() {
     return fetch(this._url + "/cards", {
       method: "GET",
-      headers: {
-        authorization: `Bearer ${localStorage.getItem("token")}`,
-        "content-type": "application/json"
-      }
+      credentials: 'include',
+      headers: this._headers
     }).then((res) => this._response(res))
   }
   _response(res) {
@@ -116,35 +17,29 @@ class Api {
     }
     return Promise.reject(`Ошибка: ${res.status}`)
   }
-  getUserInfo() {
+  getInitialUser() {
     return fetch(this._url + "/users/me", {
       method: "GET",
-      headers: {
-        authorization: `Bearer ${localStorage.getItem("token")}`,
-        "content-type": "application/json"
-      }
+      credentials: 'include',
+      headers: this._headers
     }).then((res) => this._response(res))
   }
-  setUserInfo(data) {
+  getEditUser(data) {
     return fetch(this._url + "/users/me", {
       method: "PATCH",
-      headers: {
-        authorization: `Bearer ${localStorage.getItem("token")}`,
-        "content-type": "application/json"
-      },
+      credentials: 'include',
+      headers: this._headers,
       body: JSON.stringify({
         name: data.name,
         about: data.about
       })
     }).then((res) => this._response(res))
   }
-  setUserAvatar(link) {
+  getEditAvatar(link) {
     return fetch(this._url + "/users/me/avatar", {
       method: "PATCH",
-      headers: {
-        authorization: `Bearer ${localStorage.getItem("token")}`,
-        "content-type": "application/json"
-      },
+      credentials: 'include',
+      headers: this._headers,
       body: JSON.stringify({
         avatar: link.avatar
       })
@@ -153,10 +48,8 @@ class Api {
   getAddCard(data) {
     return fetch(this._url + "/cards", {
       method: "POST",
-      headers: {
-        authorization: `Bearer ${localStorage.getItem("token")}`,
-        "content-type": "application/json"
-      },
+      credentials: 'include',
+      headers: this._headers,
       body: JSON.stringify({
         name: data.name,
         link: data.link
@@ -166,28 +59,22 @@ class Api {
   deleteCards(cardId) {
     return fetch(this._url + "/cards/" + cardId, {
       method: "DELETE",
-      headers: {
-        authorization: `Bearer ${localStorage.getItem("token")}`,
-        "content-type": "application/json"
-      }
+      credentials: 'include',
+      headers: this._headers
     }).then((res) => this._response(res))
   }
   deleteLike(cardId) {
     return fetch(this._url + "/cards/" + cardId + "/likes", {
       method: "DELETE",
-      headers: {
-        authorization: `Bearer ${localStorage.getItem("token")}`,
-        "content-type": "application/json"
-      }
+      credentials: 'include',
+      headers: this._headers
     }).then((res) => this._response(res))
   }
   addLike(cardId) {
     return fetch(this._url + "/cards/" + cardId + "/likes", {
       method: "PUT",
-      headers: {
-        authorization: `Bearer ${localStorage.getItem("token")}`,
-        "content-type": "application/json"
-      }
+      credentials: 'include',
+      headers: this._headers
     }).then((res) => this._response(res))
   }
   changeLikeCardStatus(cardId, isLiked) {
--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -3,32 +3,32 @@ export const BASE_URL = 'https://backend.domainname.nomoreparties.co';
 
 function checkResponse(res) {
     if (res.ok) {
-        return res.json();
+        return res.json(); 
     }
-    return Promise.reject(`Ошибка: ${res.status}`)
+        return Promise.reject(`Ошибка: ${res.status}`)
 }
 
 export const register = (email, password) => {
     return fetch(`${BASE_URL}/signup`, {
         method: 'POST',
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password })
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({email, password})
     })
-        .then((res) => checkResponse(res));
+    .then((res) => checkResponse(res));
 }
 
 export const authorization = (email, password) => {
     return fetch(`${BASE_URL}/signin`, {
         method: 'POST',
         credentials: 'include',
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password })
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({email, password})
     })
-        .then((res) => checkResponse(res))
-        .then((data) => {
-            localStorage.setItem("userId", data._id)
-            return data;
-        })
+    .then((res) => checkResponse(res))
+    .then((data) => {
+        localStorage.setItem("userId", data._id)
+        return data;
+    })
 }
 
 
@@ -36,9 +36,8 @@ export const tokenCheck = (token) => {
     return fetch(`${BASE_URL}/users/me`, {
         method: 'GET',
         credentials: 'include',
-        headers: {
-            "Content-Type": "application/json",
-            authorization: `Bearer ${token}`
-        }
-    }).then((res) => checkResponse(res));
+        headers: {"Content-Type": "application/json"},
+        authorization: `Bearer ${token}`
+    })
+    .then((res) => checkResponse(res));
 }
